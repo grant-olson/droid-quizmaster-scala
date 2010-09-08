6df9244e517e9e34a0776328de94ea550e1d4e56
Grant Olson <grant@johnmudhead.yoyodyne.gto>
2010-09-07 22:22:10 -0400

    More lame questions

--- src/net/grantolson/quizmaster/quizzes/movies.scala
+++ src/net/grantolson/quizmaster/quizzes/movies.scala
@@ -6,6 +6,8 @@ object movies {
   val quiz = MultipleChoiceQuiz("Movie Trivia", List[MultipleChoiceQuestion](
     MultipleChoiceQuestion("Who was the professor from Videodrome?", "Professor X", "Professor Brian O'Blivion", "Professor Einstein", "Professor Ninja", B()),
     MultipleChoiceQuestion("Harvey Keitel uttered the famous line\n 'Show me with your mouth'\n in what independent movie?", "Pulp Fiction", "Mean Streets", "Bad Lieutenant", "Albino Alligator", C()),
-    MultipleChoiceQuestion("Laurence Fishburne's first leading role\n was in what movie?", "Apocalypse Now", "The Matrix", "School Daze", "Deep Cover", D())
+    MultipleChoiceQuestion("Laurence Fishburne's first leading role\n was in what movie?", "Apocalypse Now", "The Matrix", "School Daze", "Deep Cover", D()),
+    MultipleChoiceQuestion("What movie remake copied the original shot-for-shot?", "Psycho", "Ben Hur", "The Thing", "The Ring", A()),
+    MultipleChoiceQuestion("The time-travel movie Primer has how many timelines?", "0", "1", "2", "Many", D())
     ))
 }
