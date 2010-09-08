4cd1e6d2ab4acc9a212f19731b02e66b3cc5d3cb
Grant Olson <grant@johnmudhead.yoyodyne.gto>
2010-09-07 21:37:19 -0400

    More quiz questions

--- src/net/grantolson/quizmaster/quizzes/movies.scala
+++ src/net/grantolson/quizmaster/quizzes/movies.scala
@@ -5,7 +5,7 @@ import net.grantolson.quizmaster.adts._
 object movies {
   val quiz = MultipleChoiceQuiz("Movie Trivia", List[MultipleChoiceQuestion](
     MultipleChoiceQuestion("Who was the professor from Videodrome?", "Professor X", "Professor Brian O'Blivion", "Professor Einstein", "Professor Ninja", B()),
-    MultipleChoiceQuestion("Harvey Keitel uttered the famous line\n 'Show me with your mouth'\n in what independent movie?", "Pulp Fiction", "Mean Streets", "Bad Lieutenant", "Albino Alligator", C())
+    MultipleChoiceQuestion("Harvey Keitel uttered the famous line\n 'Show me with your mouth'\n in what independent movie?", "Pulp Fiction", "Mean Streets", "Bad Lieutenant", "Albino Alligator", C()),
+    MultipleChoiceQuestion("Laurence Fishburne's first leading role\n was in what movie?", "Apocalypse Now", "The Matrix", "School Daze", "Deep Cover", D())
     ))
-
 }
--- src/net/grantolson/quizmaster/quizzes/startrek.scala
+++ src/net/grantolson/quizmaster/quizzes/startrek.scala
@@ -10,7 +10,14 @@ object starTrek {
 			 YesNoQuestion("Gates McFadden", No()),
 			 YesNoQuestion("Tasha Yar", Yes()),
 			 YesNoQuestion("B'Elanna Torres", Yes()),
-			 YesNoQuestion("Harry Kim", Yes())
+			 YesNoQuestion("Harry Kim", Yes()),
+			 YesNoQuestion("Rene Auberjonois", No()),
+			 YesNoQuestion("Jadzia Daz", Yes()),
+			 YesNoQuestion("Cirroc Lofton", No()),
+			 YesNoQuestion("Gul Dukat", Yes()),
+			 YesNoQuestion("Nana Visitor", No()),
+			 YesNoQuestion("Connor Trinneer", No()),
+			 YesNoQuestion("Hoshi Sato", Yes())
 		       ))
 
 }
