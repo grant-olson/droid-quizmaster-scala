d3cad4ab9937528bb29fd4214b65a4ba07b7c9af
Grant Olson <grant@johnmudhead.yoyodyne.gto>
2010-09-11 18:11:59 -0400

    Remove old layout code

--- src/net/grantolson/quizmaster/layouthelp.scala
+++ src/net/grantolson/quizmaster/layouthelp.scala
@@ -69,49 +69,4 @@ trait layoutHelp extends Activity {
     addToRow(button)
   }
 
-  // These will be gone
-  def addRow(v:View):TableRow = {
-    val tr = new TableRow(this)
-    tr.addView(v)
-    tr
-  }
-
-
-  def makeText(text: String): TableRow = {
-    val textBox = new TextView(this)
-    textBox.setText(text)
-    
-    addRow(textBox)
-  }
-
-  def makeAnswerButton(text: String, currentType: Answers, rightAnswer: Answers, nextAction:  QuestionType => Unit) = {
-    val button = new Button(this)
-    button.setText(text)
-    val me = this // 'this' in the anon class won't work
-    button.setOnClickListener(new View.OnClickListener {
-      def onClick(v: View) {
-	quizInfo.currentQuestion += 1
-	if (currentType == rightAnswer)
-	 {
-	   quizInfo.score += 1
-	   quizInfo.flashText = Some("\n" + goodFeedback() + "\n\n")
-	 }
-	else
-	  { quizInfo.flashText = Some("\n" + badFeedback() + "\n\n") }
-        quizInfo.getNextQuestion match {
-	  case None =>
-	    val myIntent:Intent = new Intent(me, classOf[quizScore])
-            me.startActivity(myIntent)
-	  case Some(question) => nextAction(question)
-	}
-	
-      }
-    } )
-    addRow(button)
-  }
-
-  def makeTable(): TableLayout = {
-    new TableLayout(this)
-  }
-
 }
